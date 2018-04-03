2d0c176bddb5bb2d1b92d029971840a163183335
Igor G <io.gaev@gmail.com>
2018-04-04 00:44:46 +0500

    2

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=global", "igaev", "neto1673");
+    $pdo = new PDO("mysql:host=localhost;dbname=igaev", "igaev", "neto1673");
     $pdo->exec('SET CHARACTER SET utf8');    
     
 } catch (PDOException $e) {
